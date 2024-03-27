899a272a6ebc90d7d2e1a1774d9338d1485b1518
Monica Ibarra <moniba93@hotmail.com>
2024-03-27 14:39:26 -0500

    Se ajustan inserts e instrucciones para despliegue en aws

--- documentacion/inserts.sql
+++ documentacion/inserts.sql
@@ -1,216 +1,216 @@
-INSERT INTO public.items (id, price) VALUES('MLA1', 100);
-INSERT INTO public.items (id, price) VALUES('MLA2', 210);
-INSERT INTO public.items (id, price) VALUES('MLA3', 260);
-INSERT INTO public.items (id, price) VALUES('MLA4', 80);
-INSERT INTO public.items (id, price) VALUES('MLA5', 90);
-INSERT INTO public.items (id, price) VALUES('MLA6', 200);
-INSERT INTO public.items (id, price) VALUES('MLA7', 90.03);
-INSERT INTO public.items (id, price) VALUES('MLA8', 900);
-INSERT INTO public.items (id, price) VALUES('MLA9', 410);
-INSERT INTO public.items (id, price) VALUES('MLA10', 210);
-INSERT INTO public.items (id, price) VALUES('MLA11', 280);
-INSERT INTO public.items (id, price) VALUES('MLA12', 125);
-INSERT INTO public.items (id, price) VALUES('MLA13', 225);
-INSERT INTO public.items (id, price) VALUES('MLA14', 11.1);
-INSERT INTO public.items (id, price) VALUES('MLA15', 2403.3);
-INSERT INTO public.items (id, price) VALUES('MLA16', 78900.6);
-INSERT INTO public.items (id, price) VALUES('MLA17', 200000);
-INSERT INTO public.items (id, price) VALUES('MLA18', 400985);
-INSERT INTO public.items (id, price) VALUES('MLA19', 1210);
-INSERT INTO public.items (id, price) VALUES('MLA20', 15488);
-INSERT INTO public.items (id, price) VALUES('MLA21', 7893);
-INSERT INTO public.items (id, price) VALUES('MLA22', 1540);
-INSERT INTO public.items (id, price) VALUES('MLA23', 233);
-INSERT INTO public.items (id, price) VALUES('MLA24', 110);
-INSERT INTO public.items (id, price) VALUES('MLA25', 280);
+INSERT INTO items (id, price) VALUES('MLA1', 100);
+INSERT INTO items (id, price) VALUES('MLA2', 210);
+INSERT INTO items (id, price) VALUES('MLA3', 260);
+INSERT INTO items (id, price) VALUES('MLA4', 80);
+INSERT INTO items (id, price) VALUES('MLA5', 90);
+INSERT INTO items (id, price) VALUES('MLA6', 200);
+INSERT INTO items (id, price) VALUES('MLA7', 90.03);
+INSERT INTO items (id, price) VALUES('MLA8', 900);
+INSERT INTO items (id, price) VALUES('MLA9', 410);
+INSERT INTO items (id, price) VALUES('MLA10', 210);
+INSERT INTO items (id, price) VALUES('MLA11', 280);
+INSERT INTO items (id, price) VALUES('MLA12', 125);
+INSERT INTO items (id, price) VALUES('MLA13', 225);
+INSERT INTO items (id, price) VALUES('MLA14', 11.1);
+INSERT INTO items (id, price) VALUES('MLA15', 2403.3);
+INSERT INTO items (id, price) VALUES('MLA16', 78900.6);
+INSERT INTO items (id, price) VALUES('MLA17', 200000);
+INSERT INTO items (id, price) VALUES('MLA18', 400985);
+INSERT INTO items (id, price) VALUES('MLA19', 1210);
+INSERT INTO items (id, price) VALUES('MLA20', 15488);
+INSERT INTO items (id, price) VALUES('MLA21', 7893);
+INSERT INTO items (id, price) VALUES('MLA22', 1540);
+INSERT INTO items (id, price) VALUES('MLA23', 233);
+INSERT INTO items (id, price) VALUES('MLA24', 110);
+INSERT INTO items (id, price) VALUES('MLA25', 280);
 
-INSERT INTO public.users (id) VALUES('USR1');
-INSERT INTO public.users (id) VALUES('USR2');
-INSERT INTO public.users (id) VALUES('USR3');
-INSERT INTO public.users (id) VALUES('USR4');
-INSERT INTO public.users (id) VALUES('USR5');
-INSERT INTO public.users (id) VALUES('USR6');
-INSERT INTO public.users (id) VALUES('USR7');
-INSERT INTO public.users (id) VALUES('USR8');
-INSERT INTO public.users (id) VALUES('USR9');
-INSERT INTO public.users (id) VALUES('USR10');
-INSERT INTO public.users (id) VALUES('USR11');
-INSERT INTO public.users (id) VALUES('USR12');
-INSERT INTO public.users (id) VALUES('USR13');
-INSERT INTO public.users (id) VALUES('USR14');
-INSERT INTO public.users (id) VALUES('USR15');
-INSERT INTO public.users (id) VALUES('USR16');
-INSERT INTO public.users (id) VALUES('USR17');
-INSERT INTO public.users (id) VALUES('USR18');
-INSERT INTO public.users (id) VALUES('USR19');
-INSERT INTO public.users (id) VALUES('USR20');
-INSERT INTO public.users (id) VALUES('USR21');
-INSERT INTO public.users (id) VALUES('USR22');
-INSERT INTO public.users (id) VALUES('USR23');
-INSERT INTO public.users (id) VALUES('USR24');
-INSERT INTO public.users (id) VALUES('USR25');
-INSERT INTO public.users (id) VALUES('USR26');
-INSERT INTO public.users (id) VALUES('USR27');
-INSERT INTO public.users (id) VALUES('USR28');
-INSERT INTO public.users (id) VALUES('USR29');
-INSERT INTO public.users (id) VALUES('USR30');
-INSERT INTO public.users (id) VALUES('USR31');
-INSERT INTO public.users (id) VALUES('USR32');
+INSERT INTO users (id) VALUES('USR1');
+INSERT INTO users (id) VALUES('USR2');
+INSERT INTO users (id) VALUES('USR3');
+INSERT INTO users (id) VALUES('USR4');
+INSERT INTO users (id) VALUES('USR5');
+INSERT INTO users (id) VALUES('USR6');
+INSERT INTO users (id) VALUES('USR7');
+INSERT INTO users (id) VALUES('USR8');
+INSERT INTO users (id) VALUES('USR9');
+INSERT INTO users (id) VALUES('USR10');
+INSERT INTO users (id) VALUES('USR11');
+INSERT INTO users (id) VALUES('USR12');
+INSERT INTO users (id) VALUES('USR13');
+INSERT INTO users (id) VALUES('USR14');
+INSERT INTO users (id) VALUES('USR15');
+INSERT INTO users (id) VALUES('USR16');
+INSERT INTO users (id) VALUES('USR17');
+INSERT INTO users (id) VALUES('USR18');
+INSERT INTO users (id) VALUES('USR19');
+INSERT INTO users (id) VALUES('USR20');
+INSERT INTO users (id) VALUES('USR21');
+INSERT INTO users (id) VALUES('USR22');
+INSERT INTO users (id) VALUES('USR23');
+INSERT INTO users (id) VALUES('USR24');
+INSERT INTO users (id) VALUES('USR25');
+INSERT INTO users (id) VALUES('USR26');
+INSERT INTO users (id) VALUES('USR27');
+INSERT INTO users (id) VALUES('USR28');
+INSERT INTO users (id) VALUES('USR29');
+INSERT INTO users (id) VALUES('USR30');
+INSERT INTO users (id) VALUES('USR31');
+INSERT INTO users (id) VALUES('USR32');
 
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA1', 'USR1', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA1', 'USR2', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA1', 'USR3', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA1', 'USR4', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA1', 'USR5', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA1', 'USR6', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA1', 'USR7', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA1', 'USR8', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA1', 'USR9', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA1', 'USR10', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA1', 'USR11', '2013-11-03 00:00:00-07'::timestamp);
 
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA2', 'USR1', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA2', 'USR2', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA2', 'USR3', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA2', 'USR4', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA2', 'USR5', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA2', 'USR6', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA2', 'USR7', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA2', 'USR8', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA2', 'USR9', '2013-11-03 00:00:00-07'::timestamp);
 
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA3', 'USR1', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA3', 'USR2', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA3', 'USR3', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA3', 'USR4', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA3', 'USR5', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA3', 'USR6', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA3', 'USR7', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA3', 'USR8', '2013-11-03 00:00:00-07'::timestamp);
 
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA4', 'USR1', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA4', 'USR2', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA4', 'USR3', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA4', 'USR4', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA4', 'USR5', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA4', 'USR6', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA4', 'USR7', '2013-11-03 00:00:00-07'::timestamp);
 
 
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA5', 'USR1', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA5', 'USR2', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA5', 'USR3', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA5', 'USR4', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA5', 'USR5', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA5', 'USR6', '2013-11-03 00:00:00-07'::timestamp);
 
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA6', 'USR1', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA6', 'USR2', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA6', 'USR3', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA6', 'USR4', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA6', 'USR5', '2013-11-03 00:00:00-07'::timestamp);
 
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA7', 'USR1', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA7', 'USR2', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA7', 'USR3', '2013-11-03 00:00:00-07'::timestamp);
-INSERT INTO public.bookmarks
+INSERT INTO bookmarks
 (item_id, user_id, bookmarked_date)
 VALUES('MLA7', 'USR4', '2013-11-03 00:00:00-07'::timestamp);
